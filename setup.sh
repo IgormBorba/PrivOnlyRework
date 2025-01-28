8d6607beb93988139c7df522e0836388dce928a5
Solaris Business <nicklink.lvus@gmail.com>
2025-01-27 22:21:50 -0300

    update my setup.sh for compatible with ubuntu

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Atualizar o sistema
-sudo yum update -y
+sudo apt update -y
+sudo apt upgrade -y
 
 # Instalar Apache e PHP 8.1
-sudo yum install -y httpd
-sudo amazon-linux-extras enable php8.1
-sudo yum clean metadata
-sudo yum install -y php php-cli php-common php-json php-curl php-fpm php-mysqlnd php-zip php-gd php-mbstring php-xml
+sudo apt install -y apache2
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt update -y
+sudo apt install -y php8.1 php8.1-cli php8.1-common php8.1-json php8.1-curl php8.1-fpm php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-xml
 
 # Instalar Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
@@ -15,12 +17,12 @@ php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 php -r "unlink('composer-setup.php');"
 
 # Configurar Apache
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo systemctl start apache2
+sudo systemctl enable apache2
 
 # Configurar permissões
-sudo usermod -a -G apache ec2-user
-sudo chown -R ec2-user:apache /var/www
+sudo usermod -a -G www-data $USER
+sudo chown -R $USER:www-data /var/www
 sudo chmod 2775 /var/www
 find /var/www -type d -exec sudo chmod 2775 {} \;
 find /var/www -type f -exec sudo chmod 0664 {} \;
@@ -29,4 +31,7 @@ find /var/www -type f -exec sudo chmod 0664 {} \;
 sudo mkdir -p /var/www/html/data
 sudo mkdir -p /var/www/html/logs
 sudo chmod 777 /var/www/html/data
-sudo chmod 777 /var/www/html/logs 
+sudo chmod 777 /var/www/html/logs
+
+# Reiniciar Apache para aplicar as mudanças
+sudo systemctl restart apache2
